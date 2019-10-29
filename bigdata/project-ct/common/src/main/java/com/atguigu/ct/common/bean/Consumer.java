82b39199108edeea748f9898505ce9004fe3bb0d
jeason888 <1034916408@qq.com>
2019-10-29 13:37:35 +0800

    提交成功

---
+++ bigdata/project-ct/common/src/main/java/com/atguigu/ct/common/bean/Consumer.java
@@ -0,0 +1,12 @@
+package com.atguigu.ct.common.bean;
+
+import java.io.Closeable;
+
+/**
+ * @author wangjianxin
+ * @create 2019-10-23-11:35
+ */
+public interface Consumer extends Closeable {
+    //消费数据
+    public void consume();
+}
